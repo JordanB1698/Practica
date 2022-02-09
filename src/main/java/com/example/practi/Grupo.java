37c3ebd0feacfeb45b1b514a135278ecd507efd8
JordanB1698 <jbonillapacheco@gmail.com>
2022-02-09 00:49:07 -0500

    Tercer commit

---
+++ src/main/java/com/example/practi/Grupo.java
@@ -0,0 +1,50 @@
+package com.example.practi;
+
+public class Grupo {
+
+    private int producto_id;
+    private String producto;
+    private double precio;
+    private int stock;
+    private int estado;
+
+    public int getProducto_id() {
+        return producto_id;
+    }
+
+    public void setProducto_id(int producto_id) {
+        this.producto_id = producto_id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+}
--- src/main/java/com/example/practi/MainActivity.java
+++ src/main/java/com/example/practi/MainActivity.java
@@ -2,13 +2,84 @@ package com.example.practi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    RequestQueue queue;
+    String url="https://apirestfactura.herokuapp.com/api/Producto";
+
+    List<String> datos;
+    ListView listDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //inicializar
+
+        queue = Volley.newRequestQueue(this);
+
+        GetApiData();
+        listDatos=(ListView)findViewById(R.id.listDatos);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item);
+        listDatos.setAdapter(adapter);
+
+    }
+
+    private void GetApiData(){
+
+        datos =new ArrayList<String>();
+        JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if(response.length()>0){
+                    for (int i=0;i<response.length();i++){
+                        try {
+                            JSONObject obj=response.getJSONObject(i);
+                            Grupo g=new Grupo();
+                            g.setProducto_id(Integer.parseInt(obj.get("producto_Id").toString()));
+                            g.setProducto(obj.get("producto").toString());
+                            g.setPrecio(Double.parseDouble(obj.get("precio").toString()));
+                            g.setStock(Integer.parseInt(obj.get("stock").toString()));
+                            g.setEstado(Integer.parseInt(obj.get("estado").toString()));
+
+                            datos.add(g.getProducto());
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                int x=0;
+            }
+        });
+
+        queue.add(request);
     }
 }
